374fa88ba8ec75c22998a2c818a66b36059dab78
Andrew Anastasiades <atanasta.aa@gmail.com>
2021-06-05 12:12:05 -0400

    Update rename_fields.py

--- rename_fields.py
+++ rename_fields.py
@@ -1,23 +1,34 @@
-# Field Rename dictionaries.
+# Field Map Dictionaries
 
+# Fields are pulled from commercial_df
 commercial_renamer = { #Original Field : Desired Field
-    "HS_CODE" : "hs_code",
-    "COMMODITY_DESC" : "commodity_desc",
     "GEOGRAPHY_CODE" : "country_code",
-    "GEOGRAPHY_DESC" : "",
-    "ATTRIBUTE_DESC" : "",
-    "UNIT_DESC" : "",
-    "YEAR_ID" : "",
-    "TIMEPERIOD_ID" : "",
-    "AMOUNT" : "",
-    "Direction" : "",
-    "Measure" : "",
+    "HS_CODE" : "hs_code",
+    "UNIT_DESC" : "unit_descr",
+    "Direction" : "direction",
+    "Measure" : "measure",
+    "YEAR_ID" : "year_id",
+    "TIMEPERIOD_ID" : "month_id",
+    "AMOUNT" : "amount"
 }
 
+# Fields are pulled from country_df
+country_renamer = {#Original Field : Desired Field
+    "GEOGRAPHY_CODE" : "country_code",
+    "GEOGRAPHY_DESC" : "country_name",
+}
 
+# Fields are pulled from agg_temp_df
 temp_renamer = { #Original Field : Desired Field
-    "country" : "",
-    "YEAR_ID" : "",
-    "MONTH_ID" : "",
-    "AverageTemperature" : ""
+    "country" : "country_name",
+    "YEAR_ID" : "year_id",
+    "MONTH_ID" : "month_id",
+    "AverageTemperature" : "avg_temperature"
+}
+
+# Fields are pulled from fish_type
+fish_renamer = {
+    "HS_CODE" : "hs_code",
+    "COMMODITY_DESC" : "commodity_desc",
+    "FISH_TYPE" : "fish_type"
 }
