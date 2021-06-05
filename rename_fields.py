a44efc24c184c85879a1d03fc5ac65a102265979
Andrew Anastasiades <atanasta.aa@gmail.com>
2021-06-05 11:32:04 -0400

    Update rename_fields.py

--- rename_fields.py
+++ rename_fields.py
@@ -1,10 +1,17 @@
 # Field Rename dictionaries.
 
 commercial_renamer = { #Original Field : Desired Field
+<<<<<<< Updated upstream
     "HS_CODE" : "",
     "COMMODITY_DESC" : "",
     "GEOGRAPHY_CODE" : "",
     "GEOGRAPHY_DESC" : "",
+=======
+    "HS_CODE" : "hs_code",
+    "COMMODITY_DESC" : "commodity_desc",
+    "GEOGRAPHY_CODE" : "country_code",
+    "GEOGRAPHY_DESC" : "country_name",
+>>>>>>> Stashed changes
     "ATTRIBUTE_DESC" : "",
     "UNIT_DESC" : "",
     "YEAR_ID" : "",
@@ -12,12 +19,17 @@ commercial_renamer = { #Original Field : Desired Field
     "AMOUNT" : "",
     "Direction" : "",
     "Measure" : "",
+    "FISH_TYPE" : "",
 }
+
+country_renamer = {#Original Field : Desired Field
+    "GEOGRAPHY_CODE" : "country_code",
+    "GEOGRAPHY_DESC" : "country_name",
 
 
 temp_renamer = { #Original Field : Desired Field
-    "country" : "",
-    "YEAR_ID" : "",
-    "MONTH_ID" : "",
-    "AverageTemperature" : ""
+    "country" : "country_name",
+    "YEAR_ID" : "year_id",
+    "MONTH_ID" : "month_id",
+    "AverageTemperature" : "avg_temperature"
 }
